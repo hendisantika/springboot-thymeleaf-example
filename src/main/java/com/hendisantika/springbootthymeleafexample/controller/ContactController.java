028eaff65a3fae6eecd7cb1def6cb8b2d085a0d8
Hendi Santika <hendisantika@gmail.com>
2020-02-02 12:39:37 +0700

    #22 - Add ContactController

---
+++ src/main/java/com/hendisantika/springbootthymeleafexample/controller/ContactController.java
@@ -0,0 +1,33 @@
+package com.hendisantika.springbootthymeleafexample.controller;
+
+import com.hendisantika.springbootthymeleafexample.service.ContactService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-thymeleaf-example
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 02/02/20
+ * Time: 12.39
+ */
+@Controller
+public class ContactController {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    private final int ROW_PER_PAGE = 5;
+
+    @Autowired
+    private ContactService contactService;
+
+    @Value("${msg.title}")
+    private String title;
+
+
+}
