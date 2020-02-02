8d7864939f7be9a9a35fa52bb0e59cffd09ea686
Hendi Santika <hendisantika@gmail.com>
2020-02-02 11:52:37 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springbootthymeleafexample/SpringbootThymeleafExampleApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootthymeleafexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootThymeleafExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootThymeleafExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hendisantika/springbootthymeleafexample/SpringbootThymeleafExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootthymeleafexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootThymeleafExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
