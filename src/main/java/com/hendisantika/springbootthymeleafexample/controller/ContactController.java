36693b279817753a2b9d83dfdaadc2d552d7e91d
Hendi Santika <hendisantika@gmail.com>
2020-02-02 12:41:22 +0700

    #23 - Add index in ContactController

--- src/main/java/com/hendisantika/springbootthymeleafexample/controller/ContactController.java
+++ src/main/java/com/hendisantika/springbootthymeleafexample/controller/ContactController.java
@@ -6,6 +6,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 /**
  * Created by IntelliJ IDEA.
@@ -29,5 +31,10 @@ public class ContactController {
     @Value("${msg.title}")
     private String title;
 
+    @GetMapping(value = {"/", "/index"})
+    public String index(Model model) {
+        model.addAttribute("title", title);
+        return "index";
+    }
 
 }
